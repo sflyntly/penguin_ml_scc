c6aeb5b1aa61fb83bf6a22493a6cdbafa94079e9
sflyntly <131554294+sflyntly@users.noreply.github.com>
2023-12-30 09:33:15 -0800

    Added password

--- penguins_streamlit_git.py
+++ penguins_streamlit_git.py
@@ -10,6 +10,10 @@ st.write("This app uses 6 inputs to predict the species of penguin using"
          " a model built on the Palmer Penguins dataset. Use the form below"
          " to get started!")
 
+password_guess = st.text_input('Where do penguins go to dance?')
+if password_guess != 'streamlit_password':
+    st.stop()
+
 penguin_df = pd.read_csv("penguins.csv")
 rf_pickle = open('random_forest_penguin.pickle','rb')
 map_pickle = open('output_penguin.pickle','rb')
