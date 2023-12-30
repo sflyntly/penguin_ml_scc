d17be878ecf752abb69a3671a33aa5790616ab75
sflyntly <131554294+sflyntly@users.noreply.github.com>
2023-12-30 09:25:30 -0800

    Text edits
    Made text edits

--- penguins_streamlit_git.py
+++ penguins_streamlit_git.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
 
-st.title('Penguin Classifier')
+st.title(':penguin: Penguin Classifier :penguin:')
 st.write("This app uses 6 inputs to predict the species of penguin using"
-         "a model built on the Palmer Penguins dataset. Use the form below"
-         "to get started!")
+         " a model built on the Palmer Penguins dataset. Use the form below"
+         " to get started!")
 
 penguin_df = pd.read_csv("penguins.csv")
 rf_pickle = open('random_forest_penguin.pickle','rb')
